dd3d3b71f6ced392cd931659f33dc25872504543
Anton92nd <anton92nd@gmail.com>
2014-11-04 20:32:38 +0600

    two tests added

--- Converter.cs
+++ Converter.cs
@@ -11,14 +11,37 @@ namespace Mark
 {
 	class Converter
 	{
-		static public void Convert(string fileName)
+		static public readonly string[] LineEndings = { "\r\n", "\r", "\n" };
+
+		private static string[] GetParagraphsFromText(string[] text)
 		{
-			throw new NotImplementedException();
+			return text;
+		}
+
+		private static void WriteParagraphsToStream(StreamWriter outputStream, string[] paragraphs)
+		{
+			return;
+		}
+
+		static public void Convert(string fileNameWithExtension)
+		{
+			using (var inputStream = new StreamReader(fileNameWithExtension))
+			{
+				var fileName = fileNameWithExtension.Split('.')[0];
+				var outputStream = new StreamWriter(fileName + ".html");
+				var text = inputStream.ReadToEnd().Split(LineEndings, StringSplitOptions.None);
+				var paragraphs = GetParagraphsFromText(text);
+				outputStream.WriteLine("<html>");
+				WriteParagraphsToStream(outputStream, paragraphs);
+				outputStream.WriteLine("</html>");
+				outputStream.Close();
+			}
 		}
 
 		static void Main(string[] args)
 		{
-			
+			string fileName = Console.ReadLine();
+			Convert(fileName);
 		}
 	}
 
@@ -26,10 +49,25 @@ namespace Mark
 	class Converter_should
 	{
 		[Test]
+		public void throw_exception_if_no_such_fule()
+		{
+			Assert.Throws<FileNotFoundException>(() => Converter.Convert("no_such_file.txt"));
+		}
+
+		[Test]
 		public void create_file_with_html_extension()
 		{
 			Converter.Convert("sample.txt");
 			Assert.True(File.Exists("sample.html"));
+		}
+
+		[Test]
+		public void convert_empty_file_into_file_with_html_tag()
+		{
+			Converter.Convert("empty.txt");
+			var result = new StreamReader("empty.html").ReadToEnd();
+			var expected = new StreamReader("emptyResult.txt").ReadToEnd();
+			Assert.True(expected.Equals(result));
 		}
 	}
 }
