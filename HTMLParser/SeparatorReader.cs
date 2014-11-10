ba12e0ea9efe439f6b686b2c89be6f57eb98babe
Anton92nd <anton92nd@gmail.com>
2014-11-11 00:14:51 +0600

    separators reader added

---
+++ HTMLParser/SeparatorReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mark.HTMLParser
+{
+	public class SeparatorReader : ITokenReader
+	{
+		private static readonly char[] Separators = new char[] {'.', ',', '\'', '\"', '!', '?', '%', '@',
+			'#', '$', '^', '&', '*', '/', '<', '>', '-', '=' };
+
+		private static readonly Dictionary<string, string> NeedEscape = new Dictionary<string, string>()
+		{
+			{"&", "&amp;"}, {"<", "&lt;"}, {">", "&gt;"}
+		};
+
+		public Token ReadToken(string from)
+		{
+			if (from.Length == 0)
+				return null;
+			string result = from.Substring(0, 1);
+			if (NeedEscape.ContainsKey(result))
+			{
+				result = NeedEscape[result];
+			}
+			return Separators.Contains(from[0]) ? new Token(from.Substring(0, 1), result, TokenType.Separator) : null; 
+		}
+	}
+}
---
+++ Tests/SeparatorReader_should.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Mark.HTMLParser;
+
+namespace Tests
+{
+	[TestFixture]
+	class SeparatorReader_should
+	{
+		private static readonly SeparatorReader Reader = new SeparatorReader();
+
+		[Test]
+		public void return_null_if_empty_string()
+		{
+			var result = Reader.ReadToken("");
+			Assert.IsNull(result);
+		}
+
+		[Test]
+		public void read_single_separator()
+		{
+			var result = Reader.ReadToken(",.");
+			Assert.True(new Token(",", TokenType.Separator).Equals(result));
+		}
+
+		[Test]
+		public void replace_ampersand_with_amp()
+		{
+			var result = Reader.ReadToken("&.");
+			Assert.True(new Token("&", "&amp;", TokenType.Separator).Equals(result));
+		}
+
+		[Test]
+		public void replace_less_with_lt()
+		{
+			var result = Reader.ReadToken("<.");
+			Assert.True(new Token("<", "&lt;", TokenType.Separator).Equals(result));
+		}
+
+		[Test]
+		public void replace_greater_with_gt()
+		{
+			var result = Reader.ReadToken(">we");
+			Assert.True(new Token(">", "&gt;", TokenType.Separator).Equals(result));
+		}
+	}
+}
