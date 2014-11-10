9e7fc667f7f67fe52b7603dc3dddcf951c2d7213
Anton92nd <anton92nd@gmail.com>
2014-11-10 23:52:27 +0600

    escape reader added

---
+++ HTMLParser/EscapeReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mark.HTMLParser
+{
+	public class EscapeReader : ITokenReader
+	{
+		public Token ReadToken(string from)
+		{
+			if (!from.StartsWith("\\") || from.Length < 2)
+				return null;
+			string value = from.Substring(1, 1);
+			if (value.Equals("<"))
+				value = "&lt;";
+			if (value.Equals(">"))
+				value = "&gt;";
+			return new Token(from.Substring(0, 2), value, TokenType.Separator);
+		}
+	}
+}
---
+++ Tests/EscapeReader_should.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Mark.HTMLParser;
+
+namespace Tests
+{
+	[TestFixture]
+	class EscapeReader_should
+	{
+		private static readonly EscapeReader Reader = new EscapeReader();
+
+		[Test]
+		public void return_null_if_no_backslash()
+		{
+			var result = Reader.ReadToken("abc");
+			Assert.IsNull(result);
+		}
+
+		[Test]
+		public void read_one_escaped_character()
+		{
+			var result = Reader.ReadToken("\\qwe");
+			Assert.True(new Token("\\q", "q", TokenType.Separator).Equals(result));
+		}
+
+		[Test]
+		public void replace_less_with_lt()
+		{
+			var result = Reader.ReadToken("\\<we");
+			Assert.True(new Token("\\<", "&lt;", TokenType.Separator).Equals(result));
+		}
+
+		[Test]
+		public void replace_greater_with_gt()
+		{
+			var result = Reader.ReadToken("\\>we");
+			Assert.True(new Token("\\>", "&gt;", TokenType.Separator).Equals(result));
+		}
+	}
+}
